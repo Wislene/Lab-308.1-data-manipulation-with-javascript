206170b54832b25c8ac2d0717af2ae56e0e4f739
Wislene <168476494+Wislene@users.noreply.github.com>
2024-05-23 00:04:59 -0400

    really

---
+++ Lab.js
@@ -0,0 +1,50 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+
+// //Check if all numbers are divisible by 5. 
+// cache the result in a variable.
+
+if ('n1, n2, n2 is divisible by n5') {
+  alert ('Yes');
+} else {
+  alert ('no');
+} 
+
+
+if (n1>n4) {
+  let outcome= 'true';
+} else {
+  let outcome= 'false'
+}
+console.log (isvalid)
+
+// Subtract the figitrst number from the second number.
+
+// Multiply the result by the third number.
+
+// Find the remainder of dividing the result by the fourth number.
+
+//Substraction
+
+console.log (15-10);
+
+console.log (5*20);
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. nRename the variable as appropriate.
+
+// dont understand this statement or what is it asking me to do.
+
+let distance= '1500';
+let fuel budget= '$175';
+let avg cost of fuel='$3 per gallon';
+let 55 miles per hour= '30 miles per gallon';
+let 60 miles per hour= '28 miles per gallon';
+let 75 miles per hour= '23 miles per gallon';
+
+
+I need help with this exercise 
